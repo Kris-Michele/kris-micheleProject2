8164d4446c8674220b45fde7bab1b9364363e0d6
MicheleStaffiere <michelestaffiere1@gmail.com>
2023-06-15 20:48:39 -0400

    added filtering fucntion for products

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,7 +11,6 @@ window.filteredTags = filteredTags;
 const clearButton = document.getElementById('clearButton');
 
 // Adding event listeners to the tags on page load
-
 filterBox.addEventListener('click', (e) => {
     const tag = e.target;
     if(tag.nodeName === 'LI') {
@@ -19,14 +18,15 @@ filterBox.addEventListener('click', (e) => {
             //reject
         } else {
              filteredTags.push(tag.innerText);
+             filterProductRendering(filteredTags);
         };
     };
 });
 
-//Adding the event listener to the button on page load
-
+//Adding the event listener to the clear button on page load
 clearButton.addEventListener('click', () => {
     filteredTags.length = 0;
+    filterProductRendering(filteredTags);
 })
 
 onValue(dbref,(data)=>{
@@ -87,18 +87,28 @@ const displayProducts = (productsArr,node) =>{
     });
 };
 
-// 1) Target the filter box
-// 2) Add an event listener to each item inside the filter box
-// 3) Create an empty array of selected tags
-// 4) Create a function that loops throught products and find the products with matching tags
-// 5) onClick array of selected tags will be equal to the inner text value of the button clicked(button from filter box)
-// 6) Store those matching products into a new array
-// 7) Pass the filtered products into the display products function
 
-// Create a function to handle filtering. The function takes in the parameter: filterArr. This function will loop through the database to gather all the product information. Loop through each product's tag object and compare it to the filterArr. If there is a matching filter store the product information into an array called filteredProducts. Store data from database into a variable. At the end of the function after all filtering call display products and pass the desired node to display and the filtered products array. 
+const filterProductRendering = (filtersArr) =>{
+    onValue(dbref,(data)=>{
+        const allProducts = [];
+        if(data.exists()){
+            const payload = data.val().products
+            for(let product in payload){
+                allProducts.push(payload[product]);
+            }
+        };
+        const filteredProducts = allProducts.filter((product)=>{
+            const productTags = Object.values(product.tagObj);
+            return  filtersArr.every((tag)=> productTags.includes(tag));
+        });
+        if(filteredProducts.length === 0){
+            console.log(`no matches`);
+        }else{
+            displayProducts(filteredProducts,productContainer)
+        };
+  });
+};
 
-// Assign an event listener onto the parent div, if the target is = li then store the li innerText into an array called filteredTags. Assign a special event listener that will push the tags to the array and act as the catalyst for the filtering function(). 
 
-// Put an on id on the clear button and when that button is clicked we reset the array of filtered tags to be empty and run the filtering function. And if there is no tags to passed it will = true and then rerun display products function. Ensure that both displayProducts function resets innerHTML of the container to be empty. 
 
 
